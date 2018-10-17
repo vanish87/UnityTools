3da5e605c4332411cafff7764941ab4c69baaa8a
Li Yuan <liyuan@team-lab.com>
2018-10-17 16:38:15 +0900

    add state machine example

---
+++ UnityTools/Assets/StateMachine/StateBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTools.StateMachine
+{
+    public abstract class StateBase<T>
+    {
+        abstract public void Enter(T obj);
+        abstract public void Excute(T obj);
+        abstract public void Leave(T obj);
+
+        public virtual void OnMessage(T obj, Event msg)
+        {
+            
+        }
+    }
+
+    public class ExampleState : StateBase<ExampleStateMachine>
+    {
+        public static ExampleState Instance { get { return instance; } }
+        private static readonly ExampleState instance = new ExampleState();
+        private ExampleState() { }
+
+        override public void Enter(ExampleStateMachine obj)
+        {
+        }
+
+        override public void Excute(ExampleStateMachine obj)
+        {
+        }
+
+        override public void Leave(ExampleStateMachine obj)
+        {
+        }
+    }
+
+}
---
+++ UnityTools/Assets/StateMachine/StateMachine.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace UnityTools.StateMachine
+{
+    public class ObjectStateMachine<T> : MonoBehaviour where T : class
+    {
+        [SerializeField] protected StateBase<T> currentState_;
+        [SerializeField] protected StateBase<T> globalState_;
+
+        private T owner_ = null;
+
+        /// <summary>
+        /// here, some objects may not find a owner when calling OnEnable function
+        /// because the script T may be attached later
+        /// so it is better to GetComponent again when using owner later
+        /// </summary>
+        private T Owner
+        {
+            get
+            {
+                if (this.owner_ == null)
+                {
+                    this.owner_ = this.GetComponent<T>();
+                }
+                Assert.IsNotNull(owner_);
+                return this.owner_;
+            }
+        }
+
+        /// <summary>
+        /// here, some objects may not find a owner
+        /// because the script T may be attached later
+        /// so it is better to GetComponent again when using owner later
+        /// </summary>
+        protected virtual void OnEnable()
+        {
+
+        }
+
+        protected virtual void OnDisable()
+        {
+            this.owner_ = null;
+        }
+
+        public void ChangeState(StateBase<T> newState)
+        {
+            if (this.currentState_ != null)
+            {
+                this.currentState_.Leave(this.Owner);
+            }
+
+            this.currentState_ = newState;
+
+            this.currentState_.Enter(this.Owner);
+        }
+
+        protected virtual void Update()
+        {
+            if (this.globalState_ != null)
+            {
+                this.globalState_.Excute(this.Owner);
+            }
+            if (this.currentState_ != null)
+            {
+                this.currentState_.Excute(this.Owner);
+            }
+        }
+    }
+
+
+    public class ExampleStateMachine : ObjectStateMachine<ExampleStateMachine>
+    {
+
+    }
+}
+
