652952b7bad0a02732b37481cdf3a231aa095443
Li Yuan <liyuan@team-lab.com>
2019-07-16 11:31:10 +0900

    add rgba format for sender plugin

--- Plugin/Include/Sender.h
+++ Plugin/Include/Sender.h
@@ -22,14 +22,27 @@ namespace KlakNDI
         {
             static NDIlib_video_frame_v2_t frame;
 
+			auto format = static_cast<NDIlib_FourCC_type_e>(fourCC);
+			auto byteStride = width * 4;//default is 4 bytes but uyvy is 2
+			
+			switch (format)
+			{
+			case NDIlib_FourCC_type_UYVY:
+			case NDIlib_FourCC_type_UYVA:
+				byteStride = width * 2;
+				break;
+			default:
+				break;
+			}
+
             frame.xres = width;
             frame.yres = height;
-            frame.FourCC = static_cast<NDIlib_FourCC_type_e>(fourCC);
+            frame.FourCC = format;
 			frame.frame_rate_N = 60;
 			frame.frame_rate_D = 1;
             frame.frame_format_type = NDIlib_frame_format_type_progressive;
             frame.p_data = static_cast<uint8_t*>(data);
-            frame.line_stride_in_bytes = width * 2;
+            frame.line_stride_in_bytes = byteStride;
 
             NDIlib_send_send_video_async_v2(instance_, &frame);
         }
