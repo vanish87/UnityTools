617309382574e8a691f241b266729bc66128fe4c
Yuan Li <vanish8.7@gmail.com>
2019-09-20 21:01:34 +0900

    add psot effect base

---
+++ UnityTools/Assets/Rendering/PostEffectBase.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityTools.Common;
+
+namespace UnityTools.Rendering
+{
+    public class PostEffectBase : MonoBehaviour
+    {
+        [SerializeField] protected Shader shader;
+        private AutoMaterial mat;
+
+
+        private T1 t;
+
+
+        public class Test
+        {
+            public Test()
+            {
+                Debug.Log("con");
+            }
+
+            ~Test()
+            {
+                Debug.Log("decon");
+
+            }
+        }
+
+        public class T1 : AutoObject<Material>
+        {
+            public T1(Shader s) : base(new Material(s))
+            {
+                Debug.Log("T1 new");
+            }
+            protected override void DisposeUnmanaged()
+            {
+                base.DisposeUnmanaged();
+                if (Data == null) Debug.Log("data null");
+
+                Data?.DestoryObj();
+            }
+        }
+
+        protected virtual void Start()
+        {
+            Assert.IsNotNull(this.shader);
+            this.mat = new AutoMaterial(this.shader);
+
+            //t = new T1(shader);
+        }
+
+        protected virtual void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            //Graphics.Blit(source, destination, this.mat);
+        }
+        protected void OnDestroy()
+        {
+            //t.Dispose();
+            this.mat.Dispose();
+        }
+    }
+}
---
+++ UnityTools/Assets/Rendering/Shader/PostEffectBase.shader
@@ -0,0 +1,36 @@
+﻿Shader "ImageEffect/Base"
+{
+    Properties
+    {
+        [HideInInspector]
+        _MainTex("Texture", 2D) = "white" {}
+    }
+
+	CGINCLUDE
+	#include "UnityCG.cginc"
+
+	sampler2D _MainTex;
+
+	fixed4 frag(v2f_img input) : SV_Target
+	{
+		float4 color = tex2D(_MainTex, input.uv);
+		color.rgb = 1 - color.rgb;
+
+		return color;
+	}
+
+	ENDCG
+
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag    
+            ENDCG
+        }
+    }
+}
