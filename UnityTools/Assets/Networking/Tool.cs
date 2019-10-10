321a20421a09f0fccbe0f72694ba565cd0180857
Yuan Li <vanish8.7@gmail.com>
2019-10-10 17:15:17 +0900

    add networking tools

---
+++ UnityTools/Assets/Networking/Tool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UnityTools.Networking
+{
+    public class Tool
+    {
+        public static List<string> GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            var ret = new List<string>();
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ret.Add(ip.ToString());
+                }
+            }
+            return ret;
+        }
+    }
+}
