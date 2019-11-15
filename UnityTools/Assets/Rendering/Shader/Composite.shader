d50e3b1f324fd72faa7f8a1aa020aed32fc74d4b
Yuan Li <vanish8.7@gmail.com>
2019-11-15 12:04:23 +0900

    add texture composite class

---
+++ UnityTools/Assets/Rendering/Shader/Composite.shader
@@ -0,0 +1,78 @@
+﻿Shader "Camera/CameraComposite"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+
+
+	CGINCLUDE
+	#include "UnityCG.cginc"
+
+	struct appdata
+	{
+		float4 vertex : POSITION;
+		float2 uv : TEXCOORD0;
+	};
+
+	struct v2f
+	{
+		float2 uv : TEXCOORD0;
+		float4 vertex : SV_POSITION;
+	};
+
+	sampler2D _MainTex;
+	sampler2D _TargetTex;
+	float4 _ST;
+
+	v2f vert(appdata v)
+	{
+		v2f o;
+		o.vertex = UnityObjectToClipPos(v.vertex);
+		o.uv = v.uv;
+		//o.uv.y = 1 - v.uv.y;
+		return o;
+	}
+
+	fixed4 fragComposite(v2f i) : SV_Target
+	{
+		fixed4 col = fixed4(i.uv, 0, 1);
+
+		if (i.uv.x < _ST.x || i.uv.x > _ST.x + _ST.z || i.uv.y < _ST.y || i.uv.y >_ST.y + _ST.w)
+		{
+			return fixed4(0, 0, 0, 1);
+		}
+		else
+		{
+			return tex2D(_MainTex, (i.uv - _ST.xy) / _ST.zw);
+		}
+	}
+	
+	fixed4 fragAddTarget(v2f i) : SV_Target
+	{
+		return tex2D(_TargetTex, i.uv) + tex2D(_MainTex, i.uv);
+	}
+	ENDCG
+
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+		Blend One One
+
+        Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment fragComposite			
+			ENDCG
+		}
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment fragAddTarget			
+			ENDCG
+		}
+    }
+}
---
+++ UnityTools/Assets/Rendering/TextureTools/TextureComposite.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityTools.Common;
+
+namespace UnityTools.Rendering
+{
+    public class TextureComposite : MonoBehaviour
+    {
+        [Serializable]
+        public class TextureObject
+        {
+            public Texture texture;
+            public Vector4 st;
+        }
+        public enum TextureLayout
+        {
+            Auto,
+            Horizontal,
+            Vertical,
+        }
+        [SerializeField] protected Shader shader;
+        [SerializeField] protected DisposableMaterial combineMat;
+        [SerializeField] protected RenderTexture finalOutput;
+        [SerializeField] protected List<TextureObject> textureList = new List<TextureObject>();
+
+        protected void OnEnable()
+        {
+            this.combineMat = new DisposableMaterial(this.shader);
+        }
+
+        protected void OnDisable()
+        {
+            this.combineMat.Dispose();
+        }
+
+        protected void AddTexture(Texture tex)
+        {
+            this.textureList.Add(new TextureObject() { texture = tex });
+        }
+
+        protected void CleanUp()
+        {
+            foreach( var t in this.textureList)
+            {
+                t.texture.DestoryObj();
+            }
+
+            this.textureList.Clear();
+        }
+        protected void CombineTextures()
+        {
+            this.finalOutput.Clear();
+
+            Material mat = this.combineMat;
+            foreach (var c in this.textureList)
+            {
+                mat.SetVector("_ST", c.st);
+                Graphics.Blit(c.texture, this.finalOutput, this.combineMat, 0);
+            }
+        }
+
+        protected void RecalculateTextureParameter(TextureLayout layout)
+        {
+            Assert.IsNotNull(this.textureList);
+            Assert.IsTrue(this.textureList.Count > 0);
+
+            var maxW = this.textureList.Max(c => c.texture.width);
+            var maxH = this.textureList.Max(c => c.texture.height);
+            //use max width or height as total width/height
+            //another size of texture is sum up of width/height size
+            var horizontal = layout == TextureLayout.Auto ?
+                             maxW < maxH :
+                             layout == TextureLayout.Horizontal;
+
+            var newSize = new Vector2Int(0, 0);
+            var currentSize = new Vector2Int(0, 0);
+            
+            var maxLineSize = (horizontal?maxW:maxH) * Mathf.CeilToInt(Mathf.Sqrt(this.textureList.Count));
+
+            foreach (var t in textureList)
+            {
+                if (horizontal)
+                {
+                    currentSize.x += maxW;
+                    if (currentSize.x > maxLineSize)
+                    {
+                        currentSize.x = maxW;
+                        currentSize.y += maxH;
+                    }
+                }
+                else
+                {
+                    currentSize.y += maxH;
+                    if (currentSize.y > maxLineSize)
+                    {
+                        currentSize.y = maxH;
+                        currentSize.x += maxW;
+                    }
+                }
+            }
+
+            newSize = currentSize;
+
+            if(currentSize.y > maxH)
+            {
+                if (horizontal)
+                {
+                    newSize.x = maxLineSize;
+                }
+                else
+                {
+                    newSize.y = maxLineSize;
+                }
+            }
+
+            //xy is start uv coordinates
+            //zw is texture size in uv space
+            var currentST = new Vector4(0, 0, 1, 1);
+            foreach (var t in textureList)
+            {
+                var sizeX = t.texture.width * 1f / newSize.x;
+                var sizeY = t.texture.height * 1f / newSize.y;
+
+                currentST.z = sizeX;
+                currentST.w = sizeY;
+
+                t.st = currentST;
+
+                if (horizontal)
+                {
+                    currentST.x += sizeX;
+
+                    if (currentST.x > maxLineSize / newSize.x)
+                    {
+                        currentST.x = 0;
+                        currentST.y += maxH * 1.0f / newSize.y;
+                    }
+                }
+                else
+                {
+                    currentST.y += sizeY;
+                    if (currentST.y > maxLineSize / newSize.y)
+                    {
+                        currentST.y = 0;
+                        currentST.x += maxW * 1.0f / newSize.x;
+                    }
+                }
+            }
+
+            this.finalOutput.DestoryObj();
+
+            var desc = new RenderTextureDescriptor(newSize.x, newSize.y);
+            this.finalOutput = TextureManager.Create(desc);
+            this.finalOutput.name = "FinalOutput";
+        }
+    }
+}
