dbed3375ffa2255acb919bd72518f1bb18fb1ab2
Yuan Li <vanish8.7@gmail.com>
2020-09-18 19:47:22 +0900

    add file name attributes

--- UnityTools/Assets/Attributes/DisableEditAttribute.cs
+++ UnityTools/Assets/Attributes/DisableEditAttribute.cs
@@ -8,3 +8,21 @@ namespace UnityTools.Attributes
     {
     }
 }
+namespace UnityTools.Editor
+{
+    #if UNITY_EDITOR
+    using UnityEditor;
+    using UnityTools.Attributes;
+
+    [CustomPropertyDrawer(typeof(DisableEditAttribute))]
+    public class DisableEditAttributeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+    #endif
+}
