4a86eea282f70387459818fe6573fe96fe7710f2
Yuan Li <vanish8.7@gmail.com>
2021-04-26 11:35:22 +0900

    add expreimental message socket

--- UnityTools/Assets/Networking/MessageDataSync/MessageDataSocket.cs
+++ UnityTools/Assets/Networking/MessageDataSync/MessageDataSocket.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
 using UnityTools.Common;
+using UnityTools.Debuging;
 
 namespace UnityTools.Networking
 {
@@ -29,8 +31,128 @@ namespace UnityTools.Networking
             return Serialization.ObjectToByteArray(this);
         }
     }
+    public class MessageSocket : UDPSocket<MessageSocket.Data>
+    {
+        [Serializable]
+        public class Data
+        {
+            public string hash;
+            public int messageID;
+            public short replyPort;
+            public byte[] data;
+        }
+
+        public interface IMessage
+        {
+            string hash { get; }
+            byte[] OnSerialize();
+            void OnDeserialize(byte[] data);
+
+            void OnSuccessReceived();
+        }
+
+        protected ConcurrentDictionary<string, Data> currentSendingData = new ConcurrentDictionary<string, Data>();
+        protected ConcurrentDictionary<int, Data> currentReceivedData = new ConcurrentDictionary<int, Data>();
+        protected ConcurrentDictionary<string, IMessage> messageMap = new ConcurrentDictionary<string, IMessage>();
+        protected List<SocketData> remote = new List<SocketData>();
+        protected short replayPort = -1;
+
+        protected int messageCounter = 0;
+
+        protected short GetNextAvailablePortFrom(short port = 10000)
+        {
+            var currentPorts = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().GetActiveUdpListeners();
+            while(port < short.MaxValue)
+            {
+                var np = Array.Find(currentPorts,udp=>udp.Port == port);
+                if(np == null) return port;
+
+                port++;
+            }
+            return -1;
+        }
+
+        public void SetupReply()
+        {
+            if(this.replayPort > 0) return;
+            this.replayPort = this.GetNextAvailablePortFrom();
+            this.StartReceive(this.replayPort);
+        }
+
+        public override void OnMessage(SocketData socket, Data data)
+        {
+            lock(obj)
+            {
+            if(this.replayPort > 0 && this.currentSendingData.ContainsKey(data.hash))
+            {
+                var d = default(Data);
+                this.currentSendingData.TryRemove(data.hash, out d);
+                if (this.messageMap.ContainsKey(data.hash)) this.messageMap[data.hash].OnSuccessReceived();
+            }
+            else
+            {
+                if(!this.currentReceivedData.ContainsKey(data.messageID))
+                {
+                    this.messageMap[data.hash].OnDeserialize(data.data);
+                }
+
+                this.currentReceivedData.AddOrUpdate(data.messageID, data, (mid, old) => data);
+                var server = SocketData.Make(socket.endPoint.Address, data.replyPort);
+                this.Send(server, data);
+            }
+            }
+        }
+
+        public void BindRemote(List<SocketData> sockets)
+        {
+            this.remote = sockets;
+        }
+        public void BindData(IMessage message)
+        {
+            if(this.messageMap.ContainsKey(message.hash))
+            {
+                LogTool.Log("Same hash, use different hash", LogLevel.Warning);
+                return;
+            }
+            this.messageMap.AddOrUpdate(message.hash, message, (h, old) => message);
+        }
+        public void Send(IMessage message)
+        {
+            // this.SetupReply();
+
+            var hash = message.hash;
+            var mid = this.messageCounter++%int.MaxValue;
+            var data = new Data() { hash = message.hash, messageID = mid, replyPort = this.replayPort, data = message.OnSerialize() };
+            this.currentSendingData.AddOrUpdate(hash, data, (h, old)=>data);
+        }
+
+        object obj = new object();
+
+        public void Update()
+        {
+            lock(obj)
+            {
+            foreach(var r in this.remote)
+            {
+                foreach (var d in this.currentSendingData)
+                {
+                    this.Send(r, d.Value);
+                }
+            }
+            }
+        }
+    }
     public class MessageDataSocket : UDPSocket<List<MessageDataSocket.InternalData>>
     {
+        [System.Runtime.InteropServices.DllImport("msvcrt.dll", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        static extern int memcmp(byte[] b1, byte[] b2, long count);
+
+        static bool ByteArrayCompare(byte[] b1, byte[] b2)
+        {
+            // Validate buffers are the same length.
+            // This also ensures that the count does not exceed the length of either buffer.  
+            return b1.Length == b2.Length && memcmp(b1, b2, b1.Length) == 0;
+        }
         public static string GetHash(string inputString)
         {
             HashAlgorithm algorithm = SHA256.Create();
@@ -49,7 +171,7 @@ namespace UnityTools.Networking
             // Return the hexadecimal string.
             return sBuilder.ToString();
         }
-        
+
         protected Dictionary<string, IMessageData> data = new Dictionary<string, IMessageData>();
         protected List<InternalData> sendData = new List<InternalData>();
         [Serializable]
@@ -72,7 +194,7 @@ namespace UnityTools.Networking
         {
             var hash = GetHash(hashID != null ? hashID : message.HashString);
 
-            if(!overwrite && this.data.ContainsKey(hash))
+            if (!overwrite && this.data.ContainsKey(hash))
             {
                 Debug.LogError("Same hash in the data set, set a unique name in hashID");
                 return;
@@ -102,6 +224,6 @@ namespace UnityTools.Networking
                 }
             }
         }
-        
+
     }
 }
