31d8b92c6c5d9e810faa33308ea52ef5c2733028
Yuan Li <vanish8.7@gmail.com>
2021-11-09 13:23:25 +0900

    add experimental random

--- UnityTools/Assets/Math/Math.cs
+++ UnityTools/Assets/Math/Math.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 namespace UnityTools.Math
 {
+    public class RandomExtension
+    {
+        //note: do not use readonly here
+        public static Unity.Mathematics.Random R = new Unity.Mathematics.Random((uint)System.DateTime.Now.Ticks);
+        public static float2 Rand2 => R.NextFloat2();
+        public static float3 Rand3 => R.NextFloat3();
+        public static float4 Rand4 => R.NextFloat4();
+    }
     public static class Operation
     {
         public static float2x2 OuterProduct(float2 lhs, float2 rhs)
